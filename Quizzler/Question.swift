9671ed94d017f0a84435433ec1b15551a936ec6e
garima94921 <gaarimabothra@gmail.com>
2019-11-28 17:41:24 +0530

    First commit

---
+++ Quizzler/Question.swift
@@ -0,0 +1,19 @@
+//
+//  QuestionBank.swift
+//  Quizzler
+//
+
+
+import Foundation
+
+class Question
+{
+    
+    let questionText : String
+    let answer : Bool
+    
+    init(text:String,correctAnswer:Bool){
+        questionText = text
+        answer = correctAnswer
+    }
+}
